cdf9f805f7e890bf0913ee95eb491872bade65df
Harsh Gajjar <harshgajjar280@gmail.com>
2025-05-05 19:35:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,81 @@
+# Real-Time Chat App
+
+A fully functional real-time chat application built with React and Firebase Real-Time Database. This app provides a seamless chatting experience with real-time updates, secure authentication, and a responsive user interface.
+
+
+## Features
+
+- Real-Time Messaging: Messages are instantly updated for all users in a chat.
+- User Authentication: Secure sign-up and login using Firebase Authentication.
+- Responsive Design: Optimized for mobile and desktop devices.
+- User Presence: Displays when users are online (optional feature).
+- Message Notifications: Notify users of new messages (optional).
+
+
+
+## Tech Stack
+
+**Frontend:** 
+- React: For building a dynamic and responsive user interface.
+- CSS/SCSS: For styling.
+
+**Backend:** 
+- Firebase Real-Time Database: To handle real-time data synchronization.
+- Firebase Authentication: For secure user login and registration.
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/jainilDesai/Chat-App
+```
+
+Go to the project directory
+
+```bash
+  cd chat-app
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run dev
+```
+
+
+## Environment Variables
+
+To run this project, you will need to add the following environment variables to your .env file
+
+`secerts_apikey`
+
+`secrets_authDomain` 
+
+`secrets_projectId` 
+
+`secerts_storageBucket`
+
+`secrets_messagingSenderId` 
+
+`secerts_appId` 
+## Contributing
+
+Contributions are welcome! Please fork the repository and submit a pull request.
+
+
+## Contact
+
+For support, email desaijainil9657@gmail.com or me at DM at [@jainil_desai](https://www.instagram.com/jainil_desai/) on Instagram.
+
+#
+
+Feel free to customize it further based on your Imagination and Creativity :)
+
---
+++ src/components/ChatBox/ChatBox.jsx
@@ -0,0 +1,204 @@
+import React, { useContext, useEffect, useState } from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { toast } from "react-toastify";
+import upload from "../../lib/upload";
+
+const ChatBox = () => {
+  const {
+    userData,
+    messagesId,
+    chatUser,
+    messages,
+    setMessages,
+    chatVisible,
+    setChatVisible,
+  } = useContext(AppContext);
+  const [input, setInput] = useState("");
+
+  const sendMessage = async () => {
+    try {
+      if (input && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            text: input,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error.message);
+      console.error(error);
+    }
+    setInput("");
+  };
+
+  const sendImage = async (e) => {
+    try {
+      const fileUrl = await upload(e.target.files[0]);
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = "Image";
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const convertTimestamp = (timeStamp) => {
+    return timeStamp
+      .toDate()
+      .toLocaleTimeString()
+      .replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+  };
+
+  useEffect(() => {
+    try {
+      if (messagesId) {
+        const unSub = onSnapshot(doc(db, "messages", messagesId), (res) => {
+          setMessages(res.data().messages.reverse());
+        });
+        return () => {
+          unSub();
+        };
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  }, [messagesId]);
+
+  return chatUser ? (
+    <div className={`chat-box ${chatVisible ? "" : "hidden"}`}>
+      <div className="chat-user">
+        <img src={chatUser.userData.avatar} alt="" />
+        <p>
+          {chatUser.userData.name}{" "}
+          {Date.now() - chatUser.userData.lastSeen <= 70000 ? (
+            <img className="dot" src={assets.green_dot} alt="" />
+          ) : null}
+        </p>
+        <img className="help" src={assets.help_icon} alt="" />
+        <img
+          onClick={() => setChatVisible(false)}
+          className="arrow"
+          src={assets.arrow_icon}
+          alt=""
+        />
+      </div>
+
+      <div className="chat-msg">
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={msg.sId === userData.id ? "s-msg" : "r-msg"}
+          >
+            {msg["image"] ? (
+              <img className="msg-img" src={msg.image} alt="" />
+            ) : (
+              <p className="msg">{msg.text}</p>
+            )}
+
+            <div>
+              <img
+                src={
+                  msg.sId === userData.id
+                    ? userData.avatar
+                    : chatUser.userData.avatar
+                }
+                alt=""
+              />
+              <p>{convertTimestamp(msg.createdAt)}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="chat-input">
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          type="text"
+          placeholder="Send a message"
+        />
+        <input
+          onChange={sendImage}
+          type="file"
+          id="image"
+          accept="image/png, image/jpeg"
+          hidden
+        />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img onClick={sendMessage} src={assets.send_button} alt="" />
+      </div>
+    </div>
+  ) : (
+    <div className={`chat-welcome ${chatVisible ? "" : "hidden"}`}>
+      <img src={assets.logo_icon} alt="" />
+      <p>Chat anytime, anywhere</p>
+    </div>
+  );
+};
+
+export default ChatBox;
